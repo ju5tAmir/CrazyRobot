1f6d5cf8e5e5de429fdf3804419ad7d7c6140322
Nelson  Veríssimo <nelson.sverissimo@gmail.com>
2025-04-14 00:41:13 +0200

    Added more boilerplate files like Auth and some example with websocket

---
+++ server/Api.Rest/Controllers/Websockets/SubscriptionController.cs
@@ -0,0 +1,46 @@
+using Application.Interfaces.Api.Websocket;
+using Application.Interfaces.Infrastructure.Websocket;
+using Application.Interfaces.Security;
+using Application.Models.Dtos.Rest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Rest.Controllers.Websockets;
+
+[ApiController]
+public class SubscriptionController(
+    ISecurityService securityService,
+    IConnectionManager connectionManager,
+    IWebsocketSubscriptionService websocketSubscriptionService) : ControllerBase
+{
+    public const string SubscriptionRoute = nameof(Subscribe);
+
+    public const string UnsubscribeRoute = nameof(Unsubscribe);
+
+    public const string ExampleBroadcastRoute = nameof(ExampleBroadcast);
+
+    [HttpPost]
+    [Route(SubscriptionRoute)]
+    public async Task<ActionResult> Subscribe([FromHeader] string authorization, [FromBody] ChangeSubscriptionDto dto)
+    {
+        securityService.VerifyJwtOrThrow(authorization);
+        await websocketSubscriptionService.SubscribeToTopic(dto.ClientId, dto.TopicIds);
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route(UnsubscribeRoute)]
+    public async Task<ActionResult> Unsubscribe([FromHeader] string authorization, [FromBody] ChangeSubscriptionDto dto)
+    {
+        securityService.VerifyJwtOrThrow(authorization);
+        await websocketSubscriptionService.UnsubscribeFromTopic(dto.ClientId, dto.TopicIds);
+        return Ok();
+    }
+
+    [HttpPost]
+    [Route(ExampleBroadcastRoute)]
+    public async Task<ActionResult> ExampleBroadcast([FromBody] ExampleBroadcastDto dto)
+    {
+        await connectionManager.BroadcastToTopic("ExampleTopic", dto);
+        return Ok();
+    }
+}
--- server/Infrastructure.Postgres.Scaffolding/AppDbContext.cs
+++ server/Infrastructure.Postgres.Scaffolding/AppDbContext.cs
@@ -12,70 +12,10 @@ public partial class AppDbContext : DbContext
     {
     }
 
-    public virtual DbSet<Group> Groups { get; set; }
-
-    public virtual DbSet<Message> Messages { get; set; }
-
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Group>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("group_pkay");
-
-            entity.ToTable("group", "crazyrobot");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-
-            entity.HasMany(d => d.Users).WithMany(p => p.Groups)
-                .UsingEntity<Dictionary<string, object>>(
-                    "Groupmember",
-                    r => r.HasOne<User>().WithMany()
-                        .HasForeignKey("Userid")
-                        .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("groupmember_user_fk"),
-                    l => l.HasOne<Group>().WithMany()
-                        .HasForeignKey("Groupid")
-                        .OnDelete(DeleteBehavior.ClientSetNull)
-                        .HasConstraintName("groupmember_group_fk"),
-                    j =>
-                    {
-                        j.HasKey("Groupid", "Userid").HasName("groupmember_pk");
-                        j.ToTable("groupmember", "crazyrobot");
-                        j.HasIndex(new[] { "Userid" }, "IX_groupmember_userid");
-                        j.IndexerProperty<string>("Groupid").HasColumnName("groupid");
-                        j.IndexerProperty<string>("Userid").HasColumnName("userid");
-                    });
-        });
-
-        modelBuilder.Entity<Message>(entity =>
-        {
-            entity
-                .HasNoKey()
-                .ToTable("message", "crazyrobot");
-
-            entity.HasIndex(e => e.Groupid, "IX_message_groupid");
-
-            entity.HasIndex(e => e.Userid, "IX_message_userid");
-
-            entity.Property(e => e.Groupid).HasColumnName("groupid");
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Messagetext).HasColumnName("messagetext");
-            entity.Property(e => e.Timestamp).HasColumnName("timestamp");
-            entity.Property(e => e.Userid).HasColumnName("userid");
-
-            entity.HasOne(d => d.Group).WithMany()
-                .HasForeignKey(d => d.Groupid)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("message_group_id_fk");
-
-            entity.HasOne(d => d.User).WithMany()
-                .HasForeignKey(d => d.Userid)
-                .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("message_user_id_fk");
-        });
-
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("user_pkey");
